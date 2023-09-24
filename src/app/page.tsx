72a225a17b66a3270a690f36d894456d0b39eaa0
Jjoshhh <JOSH.22@ichat.sp.edu.sg>
2023-09-24 13:47:11 +0800

    Testing Shared CDN Component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
-import {Button} from "@/components/ui/button";
+import { Button } from "@/components/ui/button";
+import { Card, CardTitle } from "@/components/ui/card";
 
 export default function Home() {
-  return <Button>Hello World</Button>;
+  return (
+    <div>
+      <Button>Hello World</Button>
+      <Card>
+        <CardTitle>Jia Jun is Gay</CardTitle>
+      </Card>
+    </div>
+  );
 }
